5cbe609e5e9fc15887e2fbd8cf175ab96d2d7ece
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-03-25 15:17:43 -0300

    navigator clipboard

---
+++ clipBoard.js
@@ -0,0 +1,19 @@
+const copyButton = document.querySelector('#copy-button')
+const input      = document.querySelector('#copy-input')
+const p          = document.querySelector('#copy-read')
+
+copyButton.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    const value = input.value
+    navigator.clipboard.writeText(value)
+})
+
+function readCopy () {
+    navigator.clipboard.readText()
+    .then (pasteText => {
+        console.log(pasteText)
+
+        if(pasteText !== '')  p.innerHTML =  `Seu último texto copiado é: ${pasteText}`
+    })
+}
+readCopy()
